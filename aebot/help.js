1cb2d8b94fab096697081842cc22137f44470943
Zanganss <76145765+Zanganss@users.noreply.github.com>
2021-02-09 19:37:20 +0800

    Update help.js

--- aebot/help.js
+++ aebot/help.js
@@ -9,7 +9,7 @@ ${yt}
 ${instagram}
 
 \`\`\`GROUP OFFICIAL\`\`\`
- https://chat.whatsapp.com/F1aEGHydcNqESvVkSlyRBp
+ https://chat.whatsapp.com/BACkgLHfblPDGPCd9ONXoA
 
 \`\`\`HARAP BACA NOTE DIATAS SEBELUM\`\`\`
 \`\`\`MENGGUNAKAN BOT\`\`\`
@@ -21,7 +21,7 @@ ${instagram}
    *Limit Kamu : ${limitt}*
 
   🚻 \`\`\`Total Pengguna:\`\`\` *${user.length} User*
-  🏧 \`\`\`Total Donasi:\`\`\` *1%* 🙂
+  🏧 \`\`\`Total Donasi:\`\`\` *0%* 🙂
 
 
                *ABOUT* ${name}
